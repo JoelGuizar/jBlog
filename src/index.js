b577241a1f47fdf43d976402f5a0308fb78a29aa
Joel Guizar <joelguizar@gmail.com>
2017-11-07 21:23:17 -0800

    refactored, added posts_index

--- src/index.js
+++ src/index.js
@@ -2,30 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-
 //browser router is what communicates with the history/url library
 //Route is a component for conditional rendering
 import { BrowserRouter, Route }from 'react-router-dom';
-
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from '/components/post_index';
+
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component {
-  render(){return <div> Hello!</div>}
-}
 
-class Goodbye extends React.Component {
-  render(){return <div> Goodbye!</div>}
-}
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/Hello" component={Hello}/>
-        <Route path="/Goodbye" component={Goodbye}/>
+        <Route path="/" component={PostsIndex}/>
       </div>
     </BrowserRouter>
   </Provider>
