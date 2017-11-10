30213c5d7aab519ce75eb1068ce57100d530f1b4
Joel Guizar <joelguizar@gmail.com>
2017-11-09 20:22:01 -0800

    redux-form added

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+//helps connect to the redux-form reducer
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render(){
     return (
-      <div>
-        PostsNew!
-      </div>
+      //redux-form still has to work with form inputs
+      <form>
+        <Field
+          //name is what piece of state is the User editing
+          name="title"
+          //takes in a function, or another component, used to display this field.
+          //Field component doesn't know how to show on the screen, it needs
+          //this component/function to show the JSX.
+          component={this.renderTitleField}
+        />
+      </form>
     );
   }
 }
 
-export default PostsNew;
+//reduxForm works just like connect
+//however, comes with configuration options
+export default reduxForm({
+  //has to be unique, so redux-form keeps form/the form's state separate
+  form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
-import PostsReducer from './reducer_posts'
+//creates form action creators for us, so we just need the reducer
+import { reducer as formReducer } from 'redux-form';
+import PostsReducer from './reducer_posts';
 
+//must be assigned as form
 const rootReducer = combineReducers({
-  posts: PostsReducer
+  posts: PostsReducer,
+  form: formReducer
 });
 
 export default rootReducer;
