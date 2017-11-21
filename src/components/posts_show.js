4e55a3b6283c21af6440b811288d53af57e39766
Joel Guizar <joelguizar@gmail.com>
2017-11-21 15:40:05 -0800

    posts_show mapStateToProps

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,43 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { fetchPost } from '../actions';
 
 class PostsShow extends Component {
+  componentDidMount(){
+    //below is provided by react router
+    //match is the top level property, and the
+    //params is all the tokens that live inside the index.js such as :id
+    //it can be /posts/:id or /posts/:id/:components
+    const { id } = this.props.params;
+    this.props.fetchPost();
+  }
+
   render(){
+    const {post} = this.props;
+
+    if (!post) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div> Your Post </div>
+      <div>
+        <h3> {post.title} </h3>
+        <h6> Categories: {post.categories} </h6>
+        <p> {post.content} </p>
+      </div>
     )
   }
 }
 
-export default PostsShow;
+//gets state from app-state to component-state
+// we only care about the posts
+// the second argument = is the props object that is going to 'this' component
+// by convention we call it OwnProps
+// we use mapStateToProps not only to grab the app state,
+//but to do specific calculations like this
+function mapStateToProps({ posts }, OwnProps) {
+  //now the component will only receive THIS particular post
+  return { post: posts[ownProps.match.params.id] }
+}
+
+export default connect(null, { fetchPost })(PostsShow);
